a55db0e08b1a3e698c3536f3c57589004bd8e7c5
Louis-Mael Gueguen <louis-mael.gueguen@crchudequebec.ulaval.ca>
2024-03-12 14:08:26 -0400

    added 2 scripts for glmnet step

---
+++ scripts/find_unaligned.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+Help() {
+echo "
+This script finds unaligned unitigs in a fasta file from a .m8 (mmseq2 output). Its arguments are :
+	-f --fasta <PATH> path to the fasta file.
+	-m --m8 <PATH> path to the mmseq2 report file in .m8 format.
+	-o --output <PATH> path to the desired output directory.
+	-h --help displays this help message and exits.
+
+It output the following files :
+	- unaligned.fasta file containing the unaligned sequences.
+"
+}
+
+if [ $# -eq 0 ]
+then
+	Help
+	exit 0
+fi
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+	-f | --fasta) fasta="$2"
+	shift 2;;
+	-m | --m8) m8="$2"
+	shift 2;;
+	-o | --output) output="$2"
+	shift 2;;
+	-h | --help) Help; exit 0;;
+	-* | --*) unknown="$1"; echo -e "ERROR: unknown argument: $unknown. Exiting."; exit 1;;
+	*) shift ;;
+	esac
+done
+
+if [ ! -e "$fasta" ]
+then
+	echo -e "ERROR: $fasta file does not exist. Exiting."
+elif [ ! -e "$m8" ]
+then
+	echo -e "ERROR: $m8 file does not exist. Exiting."
+elif [ ! -d "$output" ]
+then
+	echo -e "ERROR: $output directory does not exist or is not a directory. Exiting"
+fi
+
+#gets the index of the aligned unitigs
+# je pourrais remplacer cette etape par faire directemetn dans awk le calule
+#cut -f1 "$m8" | sort -n | uniq > "$output"/tmp.txt
+#calculates index of headers et sequence lines for aligned unitigs
+echo "Obtaining indexes of aligned unitigs to get unaligned unitgs, this is gonna take  a while ..."
+#awk -F '\t' 'BEGIN {OFS="\n"} FNR == NR {index_seq=($1+1)*2; index_header=2*($1+1)-1; to_delete[index_header]++; to_delete[index_seq]++} FNR != NR && !(FNR in to_delete) {print $0 }' "$output"/tmp.txt "$fasta" > "$output"/unaligned.unitigs.fa
+
+awk -F '\t' 'BEGIN {OFS="\n"} FNR == NR {index_seq=($1+1)*2; index_header=2*($1+1)-1; to_delete[index_header]++; to_delete[index_seq]++} FNR != NR && !(FNR in to_delete) {print $0 }' "$m8" "$fasta" > "$output"/unaligned.unitigs.fa
---
+++ scripts/matrix_unknown_kmers.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+Help() {
+echo "
+This script gets kmers counts of the top 100 unaligned unitigs. Its arguments are :
+	-i --input <PATH> path to the input file(s): unaligned unitigs sorted by aggregated pvalue. Can be several path separated by a comma: case_unaligned.aggregated.pvalue.fa,control_unaligned.aggregated.pvalue.fa
+	-o --output <PATH> path to the output folder.
+	-m --matrix <PATH> path to the matrix of kmer counts for each sample.
+	-h --help displays this help message and exits.
+
+Output:
+	- a matrix with counts of kmers in samples.
+"
+}
+
+if [ $# -eq 0 ]
+then
+	Help
+	exit 0
+fi
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+	-i | --input) input="$2"
+	shift 2;;
+	-o | --output) output="$2"
+	shift 2;;
+	-m | --matrix) matrix="$2"
+	shift 2;;
+	-h | --help) Help; exit 0;;
+	-* | --*) unknown="$1"; echo -e "ERROR: unknown argument: $unknown. Exiting."; exit 1;;
+	*) shift ;;
+	esac
+done
+
+IFS=',' read -r -a array <<< "$input"
+
+for i in "${array[@]}"
+do
+	if [  ! -e "$i" ]
+	then
+		echo "ERROR: $i does not exist. Exiting."
+	fi
+done
+
+if [ ! -e "$output" ]
+then
+	mkdir "$output"
+else
+	echo "WARNING: $output already exist"
+fi
+
+truncate -s 0 "$output"/tmp.txt
+for i in "${array[@]}"
+do
+	head -n 300 "$i" >> "$output"/tmp.txt
+done
+
+source ~/.bashrc
+mamba activate seqkit
+
+seqkit sliding --window 31 --step 1 "$output"/tmp.txt > "$output"/top_unknown_kmers.fa
+grep '^[^>]' "$output"/top_unknown_kmers.fa > "$output"/tmp.txt
+nbr_match=$(cat "$output"/tmp.txt | wc -l)
+zgrep -m "$nbr_match" -F -f "$output"/tmp.txt "$matrix" > "$output"/top_unknown_matrix.txt
