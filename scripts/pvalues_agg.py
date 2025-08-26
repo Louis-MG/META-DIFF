f6a74eb28c59329929f7f9c4acf3abbdc8a3c6bf
lmgueguen <lm.gueguen@orange.fr>
2025-08-26 14:16:19 -0400

    removed relic of the false protein, and applied Black linter

--- scripts/pvalues_agg.py
+++ scripts/pvalues_agg.py
@@ -10,192 +10,222 @@ from scipy.stats import cauchy
 
 
 def getArgs():
-	parser = argparse.ArgumentParser(description='Aggregates pvalues from kmdiff to a single p-value per unitig.')
-	parser.add_argument('-k', '--kmdiff', type=str, dest = 'kmdiff_input_path', action = 'store', required=True,
-                    help = 'Path to the kmdiff fasta file.')
-	parser.add_argument('-u', '--unitigs', type=str, dest= 'unitigs_input_path', action= 'store', required=True,
-		    help = 'Path to the unitigs fasta file.' )
-	parser.add_argument('-o', '--output', type=str, dest = 'output_path', action = 'store', required=True,
-		    help = 'Path to the output directory.')
-	parser.add_argument('-p', '--prefix', type=str, dest = 'prefix', action = 'store', required=True,
-                    help = 'String for the output file prefix.')
-	args = parser.parse_args()
-	return(args)
+    parser = argparse.ArgumentParser(
+        description="Aggregates pvalues from kmdiff to a single p-value per unitig."
+    )
+    parser.add_argument(
+        "-k",
+        "--kmdiff",
+        type=str,
+        dest="kmdiff_input_path",
+        action="store",
+        required=True,
+        help="Path to the kmdiff fasta file.",
+    )
+    parser.add_argument(
+        "-u",
+        "--unitigs",
+        type=str,
+        dest="unitigs_input_path",
+        action="store",
+        required=True,
+        help="Path to the unitigs fasta file.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        dest="output_path",
+        action="store",
+        required=True,
+        help="Path to the output directory.",
+    )
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        type=str,
+        dest="prefix",
+        action="store",
+        required=True,
+        help="String for the output file prefix.",
+    )
+    args = parser.parse_args()
+    return args
 
 
 def verif_input(path: Union[str, bytes, os.PathLike]):
-	"""
-	Verifies that the input path is valid.
-	:param path: Path to the input file.
-	:return: nothing
-	"""
-	path = str(path)
-	if not os.path.isfile(path):
-		print(f"ERROR: file {path} does not exist or is not a file.")
-		sys.exit()
+    """
+    Verifies that the input path is valid.
+    :param path: Path to the input file.
+    :return: nothing
+    """
+    path = str(path)
+    if not os.path.isfile(path):
+        print(f"ERROR: file {path} does not exist or is not a file.")
+        sys.exit()
 
 
 def verif_output(path: Union[str, bytes, os.PathLike], prefix: str):
-	"""
-	Checks that the output directory exists and that no file with output name exists already.
-	:param path : string or path-like object to the output directory.
-	:param prefix : string for the output file prefix.
-	:return: nothing
-	"""
-	path = str(path)
-	if not os.path.isdir(path) :
-		print(f"ERROR: folder {path} not found.")
-		sys.exit()
-	if os.path.isfile(path+"/"+prefix+".aggregated.fa") :
-		print(f"ERROR: folder {path} contains a '{prefix}.aggregated.fa' file already.")
-		sys.exit()
+    """
+    Checks that the output directory exists and that no file with output name exists already.
+    :param path : string or path-like object to the output directory.
+    :param prefix : string for the output file prefix.
+    :return: nothing
+    """
+    path = str(path)
+    if not os.path.isdir(path):
+        print(f"ERROR: folder {path} not found.")
+        sys.exit()
+    if os.path.isfile(path + "/" + prefix + ".aggregated.fa"):
+        print(f"ERROR: folder {path} contains a '{prefix}.aggregated.fa' file already.")
+        sys.exit()
 
 
-def write_output(path: Union[str, bytes, os.PathLike], list_unitigs: list[object], prefix: str):
-	"""
-	Writes output.
-	:param path : string or path-like object to the output directory.
-	:param list_unitigs : list of objects Unitig.
-	:param prefix : string for the output file prefix.
-	:return: nothing
-	"""
-	with open(path+"/"+prefix+".aggregated.fa", 'w') as f:
-		for i in list_unitigs:
-			f.write(i.header+"_pval="+str(i.pvalue))
-			f.write("\n"+i.sequence)
+def write_output(
+    path: Union[str, bytes, os.PathLike], list_unitigs: list[object], prefix: str
+):
+    """
+    Writes output.
+    :param path : string or path-like object to the output directory.
+    :param list_unitigs : list of objects Unitig.
+    :param prefix : string for the output file prefix.
+    :return: nothing
+    """
+    with open(path + "/" + prefix + ".aggregated.fa", "w") as f:
+        for i in list_unitigs:
+            f.write(i.header + "_pval=" + str(i.pvalue))
+            f.write("\n" + i.sequence)
 
 
 def load_pvalue_dict(path: Union[str, bytes, os.PathLike]) -> dict[str, float]:
-	"""
-	Loads the fasta file from kmdiff output as a dictionary :  {'sequence':'p-value'} and returns the dictionary.
-	:param path: string or path-like object.
-	:return: A dictionary containing kmers and their pvalues.
-	"""
-	kmer_2_pvalue = dict()
-	with open(path) as f :
-		for line in f :
-			if line.startswith(">") :
-				temp_str = re.sub('.*pval=', '', line)
-				temp_str = re.sub('_control=.*', '', temp_str)
-				pvalue = float(temp_str)
-			else :
-				sequence = line.rstrip('\n')
-				try :
-					kmer_2_pvalue[str(sequence)] = pvalue
-				except MemoryError :
-					size = sys.getsizeof(kmer_2_pvalue)
-					size += sum(map(sys.getsizeof, kmer_2_pvalue.values())) + sum(map(sys.getsizeof, kmer_2_pvalue.keys()))
-					print(size)
-					exit()
-	return kmer_2_pvalue
+    """
+    Loads the fasta file from kmdiff output as a dictionary :  {'sequence':'p-value'} and returns the dictionary.
+    :param path: string or path-like object.
+    :return: A dictionary containing kmers and their pvalues.
+    """
+    kmer_2_pvalue = dict()
+    with open(path) as f:
+        for line in f:
+            if line.startswith(">"):
+                temp_str = re.sub(".*pval=", "", line)
+                temp_str = re.sub("_control=.*", "", temp_str)
+                pvalue = float(temp_str)
+            else:
+                sequence = line.rstrip("\n")
+                try:
+                    kmer_2_pvalue[str(sequence)] = pvalue
+                except MemoryError:
+                    size = sys.getsizeof(kmer_2_pvalue)
+                    size += sum(map(sys.getsizeof, kmer_2_pvalue.values())) + sum(
+                        map(sys.getsizeof, kmer_2_pvalue.keys())
+                    )
+                    print(size)
+                    exit()
+    return kmer_2_pvalue
 
 
-def reverse_complement(kmer: str) -> str :
-	"""
-	Reverse complement a kmer.
-	:param kmer: string
-	:return: reversed complement string of the kmer.
-	"""
-	dict_reverse_nucleotides = {"A":"T", "T":"A", "C":"G", "G":"C"}
-	return ''.join(dict_reverse_nucleotides[ch] for ch in reversed(kmer))
+def reverse_complement(kmer: str) -> str:
+    """
+    Reverse complement a kmer.
+    :param kmer: string
+    :return: reversed complement string of the kmer.
+    """
+    dict_reverse_nucleotides = {"A": "T", "T": "A", "C": "G", "G": "C"}
+    return "".join(dict_reverse_nucleotides[ch] for ch in reversed(kmer))
 
 
 class Unitig(object):
-	"""
-	Object representing an unitig, with its sequence and aggregated pvalue.
-	"""
-	sequence : ""
-	pvalue : 0
-	kmer_pvalues : []
-	header : ""
-	def __init__(self, sequence, pvalue, kmer_pvalues, header) :
-		self.sequence = sequence
-		self.pvalue = pvalue
-		self.kmer_pvalues = kmer_pvalues
-		self.header = header
+    """
+    Object representing an unitig, with its sequence and aggregated pvalue.
+    """
+
+    sequence: ""
+    pvalue: 0
+    kmer_pvalues: []
+    header: ""
+
+    def __init__(self, sequence, pvalue, kmer_pvalues, header):
+        self.sequence = sequence
+        self.pvalue = pvalue
+        self.kmer_pvalues = kmer_pvalues
+        self.header = header
 
 
 def make_unitig(sequence: str, kmer_dict: dict[str, float], header: str) -> object:
-	"""
-	Takes a sequence and/or a pvalue to build and return an object of class unitig.
-	sequence : string
-	pvalue : float
-	kmer_pvalues : list of floats
-	:return: an object of class Unitig
-	"""
-	list_kmer_pvalues = []
-	for i in range(0, len(sequence)-31):
-		kmer = str(sequence[i:i+31])
-		try :
-			list_kmer_pvalues.append(kmer_dict[kmer])
-		except KeyError :
-			rev_compl_kmer = reverse_complement(kmer)
-			list_kmer_pvalues.append(kmer_dict[rev_compl_kmer])
-	unitig = Unitig(sequence, 0, list_kmer_pvalues, header)
-	return unitig
+    """
+    Takes a sequence and/or a pvalue to build and return an object of class unitig.
+    sequence : string
+    pvalue : float
+    kmer_pvalues : list of floats
+    :return: an object of class Unitig
+    """
+    list_kmer_pvalues = []
+    for i in range(0, len(sequence) - 31):
+        kmer = str(sequence[i : i + 31])
+        try:
+            list_kmer_pvalues.append(kmer_dict[kmer])
+        except KeyError:
+            rev_compl_kmer = reverse_complement(kmer)
+            list_kmer_pvalues.append(kmer_dict[rev_compl_kmer])
+    unitig = Unitig(sequence, 0, list_kmer_pvalues, header)
+    return unitig
 
 
-def load_unitigs(path: Union[str, bytes, os.PathLike], kmer_dict: dict[str, float]) -> list[object]:
-	"""
-	Loads unitigs as objects from the unitigs fasta file.
-	:param path : string or path-like object.
-	:param kmer_dict : dictionary of kmers and their corresponding pvalues.
-	:return: list of objects of class Unitig with aggregated pvalues.
-	"""
-	list_unitigs = []
-	with open(path) as f :
-		for line in f :
-			if line.startswith(">") :
-				header = line.rstrip('\n')
-			else :
-				sequence = line
-				#if the sequence corresponds to the fake sequence, just ignores it.
-				if header == ">ref-NC-003210.1-209461-211401-ListeriaMonocytogenes-actin-assembly-inducing-protein-precursor" :
-						pass
-				else :
-					unitig = make_unitig(sequence=sequence, kmer_dict=kmer_dict, header=header)
-					unitig_CCT = CCT(unitig)
-					list_unitigs.append(unitig_CCT)
-	return list_unitigs
+def load_unitigs(
+    path: Union[str, bytes, os.PathLike], kmer_dict: dict[str, float]
+) -> list[object]:
+    """
+    Loads unitigs as objects from the unitigs fasta file.
+    :param path : string or path-like object.
+    :param kmer_dict : dictionary of kmers and their corresponding pvalues.
+    :return: list of objects of class Unitig with aggregated pvalues.
+    """
+    list_unitigs = []
+    with open(path) as f:
+        for line in f:
+            if line.startswith(">"):
+                header = line.rstrip("\n")
+            else:
+                sequence = line
+                unitig = make_unitig(
+                    sequence=sequence, kmer_dict=kmer_dict, header=header
+                )
+                unitig_CCT = CCT(unitig)
+                list_unitigs.append(unitig_CCT)
+    return list_unitigs
 
 
 def CCT(unitig: object) -> object:
-	"""
-	Calculates the CCT of a unitig.
-	:param unitig: an object of class Unitig
-	:return: object of class Unitig with aggregated pvalues.
-	"""
-	cauchy_values = [tan((0.5-x)*pi) for x in unitig.kmer_pvalues]
-	cauchy_stat = sum(cauchy_values)/len(cauchy_values)
-	unitig.pvalue = 1-cauchy.cdf(cauchy_stat, loc=0, scale=1)
-	return unitig
+    """
+    Calculates the CCT of a unitig.
+    :param unitig: an object of class Unitig
+    :return: object of class Unitig with aggregated pvalues.
+    """
+    cauchy_values = [tan((0.5 - x) * pi) for x in unitig.kmer_pvalues]
+    cauchy_stat = sum(cauchy_values) / len(cauchy_values)
+    unitig.pvalue = 1 - cauchy.cdf(cauchy_stat, loc=0, scale=1)
+    return unitig
 
 
-#def aggregate_pvalues(list_unitigs: list[object]) -> list[object]:
-#	"""
-#	Tranforms the pvalues of kmers of each unitigs into the unitigs' pvalues.
-#	list_unitigs: list of objects Unitigs
-#	"""
-#	list_modified_unitigs = [CCT(unitig) for unitig in list_unitigs]
-#	list_modified_unitigs.sort(key = lambda x: (x.pvalue, len(x.sequence)))
-#	return list_modified_unitigs
-
-
-def main(kmdiff_input_path: Union[str, bytes, os.PathLike], unitigs_input_path: Union[str, bytes, os.PathLike], output_path: Union[str, bytes, os.PathLike], prefix: str):
-	"""
-	Main function for running pvalues aggregation on unitigs.
-	"""
-	verif_input(kmdiff_input_path)
-	verif_input(unitigs_input_path)
-	verif_output(output_path, prefix)
-	kmer_dict = load_pvalue_dict(kmdiff_input_path)
-	list_unitigs_with_pvalues = load_unitigs(unitigs_input_path, kmer_dict)
-	#list_unitigs_with_pvalues = aggregate_pvalues(list_unitigs) si marche pas remettre et la ligne au dessus est juste list_unitigs, et enlever la ligne en dessous
-	list_unitigs_with_pvalues.sort(key = lambda x: (x.pvalue, -len(x.sequence)))
-	write_output(output_path, list_unitigs_with_pvalues, prefix)
-	print("Aggregation done !")
+def main(
+    kmdiff_input_path: Union[str, bytes, os.PathLike],
+    unitigs_input_path: Union[str, bytes, os.PathLike],
+    output_path: Union[str, bytes, os.PathLike],
+    prefix: str,
+):
+    """
+    Main function for running pvalues aggregation on unitigs.
+    """
+    verif_input(kmdiff_input_path)
+    verif_input(unitigs_input_path)
+    verif_output(output_path, prefix)
+    kmer_dict = load_pvalue_dict(kmdiff_input_path)
+    list_unitigs_with_pvalues = load_unitigs(unitigs_input_path, kmer_dict)
+    list_unitigs_with_pvalues.sort(key=lambda x: (x.pvalue, -len(x.sequence)))
+    write_output(output_path, list_unitigs_with_pvalues, prefix)
+    print("Aggregation done !")
 
 
 if __name__ == "__main__":
-	args = getArgs()
-	main(args.kmdiff_input_path, args.unitigs_input_path, args.output_path, args.prefix)
+    args = getArgs()
+    main(args.kmdiff_input_path, args.unitigs_input_path, args.output_path, args.prefix)
--- scripts/sumup_table.py
+++ scripts/sumup_table.py
@@ -7,7 +7,6 @@ from typing import Dict, Union, List, Tuple
 import os
 
 
-
 def check_output(path: Union[str, bytes, os.PathLike]):
     """
     Checks if output directory exists and makes it if necessary.
@@ -19,7 +18,11 @@ def check_output(path: Union[str, bytes, os.PathLike]):
         os.makedirs(path)
 
 
-def check_input(annot_path: Union[str, bytes, os.PathLike], gene_seq_path: Union[str, bytes, os.PathLike], unitigs_path: Union[str, bytes, os.PathLike]):
+def check_input(
+    annot_path: Union[str, bytes, os.PathLike],
+    gene_seq_path: Union[str, bytes, os.PathLike],
+    unitigs_path: Union[str, bytes, os.PathLike],
+):
     """
     Checks if input files exist.
     :param annot_path: path to [case,control]_protein_translation.faa.annot file.
@@ -32,7 +35,9 @@ def check_input(annot_path: Union[str, bytes, os.PathLike], gene_seq_path: Union
             exit(1)
 
 
-def get_gene_header_to_gene_function_dict(annot_path: Union[str, bytes, os.PathLike]) -> Dict[str, List[str]]:
+def get_gene_header_to_gene_function_dict(
+    annot_path: Union[str, bytes, os.PathLike],
+) -> Dict[str, List[str]]:
     """
     Builds the dictionary of gene headers to their KO number and function.
     :param annot_path: path to [case,control]_protein_translation.faa.annot file.
@@ -41,11 +46,16 @@ def get_gene_header_to_gene_function_dict(annot_path: Union[str, bytes, os.PathL
     gene_to_function_dict = {}
     with open(annot_path, "r") as f:
         for line in f:
-            gene_to_function_dict[line.strip().split("\t")[0]] = [line.strip().split("\t")[3], line.strip().split("\t")[4]]
+            gene_to_function_dict[line.strip().split("\t")[0]] = [
+                line.strip().split("\t")[3],
+                line.strip().split("\t")[4],
+            ]
     return gene_to_function_dict
 
 
-def get_gene_header_to_gene_seq_dict(gene_seq_path: Union[str, bytes, os.PathLike]) -> Dict[str, str]:
+def get_gene_header_to_gene_seq_dict(
+    gene_seq_path: Union[str, bytes, os.PathLike],
+) -> Dict[str, str]:
     """
     Builds the dictionary of gene headers to their translated sequence.
     :param gene_seq_path: path to [case,control]_protein_translation.faa file.
@@ -54,18 +64,20 @@ def get_gene_header_to_gene_seq_dict(gene_seq_path: Union[str, bytes, os.PathLik
     gene_header_to_gene_seq_dict = {}
     with open(gene_seq_path, "r") as f:
         for record in SeqIO.parse(gene_seq_path, "fasta"):
-            gene_header_to_gene_seq_dict[record.id.rstrip('#')] = record.seq
+            gene_header_to_gene_seq_dict[record.id.rstrip("#")] = record.seq
     return gene_header_to_gene_seq_dict
 
 
-def get_clade_and_unitigs(kraken_output_path: Union[str, bytes, os.PathLike]) -> Tuple[dict[str, str], dict[str, int]]:
+def get_clade_and_unitigs(
+    kraken_output_path: Union[str, bytes, os.PathLike],
+) -> Tuple[dict[str, str], dict[str, int]]:
     """
     Builds a dictionary of unitigs to the clade they were assigned to by Kraken2
     :param kraken_output_path: path to the .output kraken output file
     """
     clade_align_base = {}
     unitigs_to_clade = {}
-    with (open(kraken_output_path, "r")) as f:
+    with open(kraken_output_path, "r") as f:
         for line in f:
             if line.startswith("U"):
                 unitigs_to_clade[line.split("\t")[1]] = "Unclassified"
@@ -73,9 +85,9 @@ def get_clade_and_unitigs(kraken_output_path: Union[str, bytes, os.PathLike]) ->
                     clade_align_base["Unclassified"] += int(line.strip().split("\t")[3])
                 except KeyError:
                     clade_align_base["Unclassified"] = int(line.strip().split("\t")[3])
-            else :
+            else:
                 unitigs_to_clade[line.split("\t")[1]] = line.split("\t")[2]
-                try :
+                try:
                     clade_align_base[line.split("\t")[2]] += int(line.split("\t")[3])
                 except KeyError:
                     clade_align_base[line.split("\t")[2]] = int(line.split("\t")[3])
@@ -94,7 +106,14 @@ def get_unitigs_dict(unitigs_path: Union[str, bytes, os.PathLike]) -> Dict[str, 
         # removes the unitigs that was added for the functional annotation
     return unitigs_dict
 
-def write_output_gene_table(path_output: Union[str, bytes, os.PathLike], gene_header_to_gene_function_dict: Dict[str, List[str]], unitigs_dict: Dict[str, str], gene_header_to_gene_seq_dict, unitigs_to_clade_dict: Dict[str, str]):
+
+def write_output_gene_table(
+    path_output: Union[str, bytes, os.PathLike],
+    gene_header_to_gene_function_dict: Dict[str, List[str]],
+    unitigs_dict: Dict[str, str],
+    gene_header_to_gene_seq_dict,
+    unitigs_to_clade_dict: Dict[str, str],
+):
     """
     Writes tab-separated output file to the output directory. Format is gene header, gene translated sequence, corresponding
     unitig header, unitig sequence, KO number, gene function.
@@ -104,49 +123,97 @@ def write_output_gene_table(path_output: Union[str, bytes, os.PathLike], gene_he
     :param gene_header_to_gene_seq_dict: dictionary of gene headers to their translated sequence.
     """
     with open(path_output, "w") as f:
-        f.write(f"{'gene_header'}\t{'gene_translated_seq'}\t{'unitig_header'}\t{'unitig_seq'}\t{'gene_KO'}\t{'gene_function'}\t{'unitig_clade'}\n")
+        f.write(
+            f"{'gene_header'}\t{'gene_translated_seq'}\t{'unitig_header'}\t{'unitig_seq'}\t{'gene_KO'}\t{'gene_function'}\t{'unitig_clade'}\n"
+        )
         for gene in gene_header_to_gene_seq_dict.keys():
             try:
-                unitig_header = gene.split('_')[0]
-                f.write(f"{gene}\t{gene_header_to_gene_seq_dict[gene]}\t{unitig_header}\t{unitigs_dict[unitig_header]}\t{gene_header_to_gene_function_dict[gene][0]}\t{gene_header_to_gene_function_dict[gene][1]}\t{unitigs_to_clade_dict[unitig_header]}\n")
+                unitig_header = gene.split("_")[0]
+                f.write(
+                    f"{gene}\t{gene_header_to_gene_seq_dict[gene]}\t{unitig_header}\t{unitigs_dict[unitig_header]}\t{gene_header_to_gene_function_dict[gene][0]}\t{gene_header_to_gene_function_dict[gene][1]}\t{unitigs_to_clade_dict[unitig_header]}\n"
+                )
             except KeyError:
-                f.write(f"{gene}\t{gene_header_to_gene_seq_dict[gene]}\t{unitig_header}\t{unitigs_dict[unitig_header]}\t{'NA'}\t{'NA'}\t{unitigs_to_clade_dict[unitig_header]}\n")
+                f.write(
+                    f"{gene}\t{gene_header_to_gene_seq_dict[gene]}\t{unitig_header}\t{unitigs_dict[unitig_header]}\t{'NA'}\t{'NA'}\t{unitigs_to_clade_dict[unitig_header]}\n"
+                )
     print(f"Output written to {path_output}")
 
-def write_output_clades_ordered(path_output: Union[str, bytes, os.PathLike], clades_to_align_length_dict: Dict[str, int], condition: str):
+
+def write_output_clades_ordered(
+    path_output: Union[str, bytes, os.PathLike],
+    clades_to_align_length_dict: Dict[str, int],
+    condition: str,
+):
     """
     Writes the output with clades ordered by total alignment length of unitigs.
     :param path_output: path to output directory.
     :param clades_to_align_length_dict: dictionary of clades to their alignment length.
     """
     with open(path_output, "w") as f:
-        sorted_clades = sorted(clades_to_align_length_dict.items(), key=lambda x: x[1], reverse=True)
+        sorted_clades = sorted(
+            clades_to_align_length_dict.items(), key=lambda x: x[1], reverse=True
+        )
         for clade in sorted_clades:
             f.write(f"{clade[0]}\t{clade[1]}\n")
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--annot', required=True, type=str, help='Genes annotation file.')
-    parser.add_argument('-g', '--gene_translation_seq', required=True, type=str, help='Gene translation file.')
-    parser.add_argument('-o', '--output', required=True, type=str, help='Output folder.')
-    parser.add_argument('-u', '--unitigs', required=True, type=str, help='Unitigs file.')
-    parser.add_argument('-c', '--case', required=True, type=str, help='Case or Control condition.')
-    parser.add_argument('-k', '--kraken_output', required=True, type=str, help='Kraken2 classification output file.')
+    parser.add_argument(
+        "-a", "--annot", required=True, type=str, help="Genes annotation file."
+    )
+    parser.add_argument(
+        "-g",
+        "--gene_translation_seq",
+        required=True,
+        type=str,
+        help="Gene translation file.",
+    )
+    parser.add_argument(
+        "-o", "--output", required=True, type=str, help="Output folder."
+    )
+    parser.add_argument(
+        "-u", "--unitigs", required=True, type=str, help="Unitigs file."
+    )
+    parser.add_argument(
+        "-c", "--case", required=True, type=str, help="Case or Control condition."
+    )
+    parser.add_argument(
+        "-k",
+        "--kraken_output",
+        required=True,
+        type=str,
+        help="Kraken2 classification output file.",
+    )
     args = parser.parse_args()
 
     check_input(args.annot, args.gene_translation_seq, args.unitigs)
     check_output(args.output)
 
-
     gene_header_to_gene_function = get_gene_header_to_gene_function_dict(args.annot)
-    gene_header_to_gene_seq = get_gene_header_to_gene_seq_dict(args.gene_translation_seq)
+    gene_header_to_gene_seq = get_gene_header_to_gene_seq_dict(
+        args.gene_translation_seq
+    )
     unitigs = get_unitigs_dict(args.unitigs)
     unitigs_to_clade, clade_base_align = get_clade_and_unitigs(args.kraken_output)
 
-    output_table_path = args.output + "/" + args.case + "_unitigs_to_clade_and_gene_functions.tsv"
+    output_table_path = (
+        args.output + "/" + args.case + "_unitigs_to_clade_and_gene_functions.tsv"
+    )
     output_clades_path = args.output + "/" + args.case + "_clades.tsv"
-    write_output_gene_table(output_table_path, gene_header_to_gene_function_dict=gene_header_to_gene_function, unitigs_dict=unitigs, gene_header_to_gene_seq_dict=gene_header_to_gene_seq, unitigs_to_clade_dict=unitigs_to_clade)
-    write_output_clades_ordered(output_clades_path, clades_to_align_length_dict=clade_base_align, condition=args.case)
+    write_output_gene_table(
+        output_table_path,
+        gene_header_to_gene_function_dict=gene_header_to_gene_function,
+        unitigs_dict=unitigs,
+        gene_header_to_gene_seq_dict=gene_header_to_gene_seq,
+        unitigs_to_clade_dict=unitigs_to_clade,
+    )
+    write_output_clades_ordered(
+        output_clades_path,
+        clades_to_align_length_dict=clade_base_align,
+        condition=args.case,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
